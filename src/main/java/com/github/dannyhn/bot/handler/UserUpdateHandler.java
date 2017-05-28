cd7c8e05640002f9826efba09e96f3e73f25bca9
Danny Nguyen <dann.ngu@gmail.com>
2017-05-28 11:54:43 -0700

    removed unused imports

--- src/main/java/com/github/dannyhn/bot/handler/UserUpdateHandler.java
+++ src/main/java/com/github/dannyhn/bot/handler/UserUpdateHandler.java
@@ -4,14 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.github.dannyhn.bot.data.User;
-import com.github.dannyhn.bot.service.MessageService;
-import com.github.dannyhn.sqlite.client.Person;
-import com.github.dannyhn.sqlite.client.SqliteClient;
 import com.github.dannyhn.sqlite.client.SqliteObjectClient;
 
 import sx.blah.discord.handle.obj.IUser;
-import sx.blah.discord.handle.obj.Status;
-import sx.blah.discord.handle.obj.Status.StatusType;
 
 /**
  * @author Danny
@@ -31,7 +26,7 @@ public class UserUpdateHandler {
 			dbUser.setId(id);
 		}
 		dbUser.setAvatarUrl(user.getAvatarURL());
-		System.out.println("User: " + user.getName() + " has new Avatar: " + user.getAvatarURL());
+		System.out.println("User Status Changed: " + user.getName());
 	}
 	
 }
