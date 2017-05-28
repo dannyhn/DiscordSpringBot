e2e4c5a4e1d235514a860e75359c3ad01b1254c6
Danny Nguyen <dann.ngu@gmail.com>
2017-05-28 11:34:53 -0700

    reduce number of requests for dota messages

--- src/main/java/com/github/dannyhn/bot/message/handler/DotaMessageHandler.java
+++ src/main/java/com/github/dannyhn/bot/message/handler/DotaMessageHandler.java
@@ -31,40 +31,41 @@ public class DotaMessageHandler implements MessageHandler {
 
 	@Autowired
 	private MessageService messageService;
-	
+
 	@Autowired
 	private SqliteObjectClient<User> sqliteClient;
-	
+
 	@Override
 	public void handleMessage(IMessage message) {
-		IUser user = message.getAuthor();
+		List<IUser> mentions = message.getMentions();
+		if (mentions.isEmpty()) {
+			handleMessageForUser(message.getAuthor(), message);
+		} else {
+			handleMessageForUserList(mentions, message);
+		}
+	}
+
+	private void handleMessageForUser(IUser user, IMessage message) {
 		User userFromDB = sqliteClient.read(user.getID(), User.class);
-		if (userFromDB == null || StringUtils.isBlank(userFromDB.getDotaUrl()))
-		{
-			messageService.sendMessage(message.getChannel(),"You are not important enough", message, true);
+		if (userFromDB == null || StringUtils.isBlank(userFromDB.getDotaUrl())) {
+			messageService.sendMessage(message.getChannel(), user.getName() + " is not important enough", message,
+					true);
+		} else {
+			messageService.sendMessage(message.getChannel(), userFromDB.getDotaUrl(), message, true);
 		}
-		else
-		{
-			messageService.sendMessage(message.getChannel(),userFromDB.getDotaUrl(), message, true);
+	}
+	
+	private void handleMessageForUserList(List<IUser> users, IMessage message) {
+		String messageToSend = "";
+		for (IUser user : users) {
+			User userFromDB = sqliteClient.read(user.getID(), User.class);
+			if (userFromDB == null || StringUtils.isBlank(userFromDB.getDotaUrl())) {
+				messageToSend += user.getName() + " is not important enough\n";
+			} else {
+				messageToSend += userFromDB.getDotaUrl() + "\n";
+			}
 		}
-		/*
-		if ("209171660224462859".equalsIgnoreCase(user.getID()))
-		{
-			messageService.sendMessage(message.getChannel(),"https://pubg.me/MasterTactioner", message, true);
-		}
-		else if ("219038305801601024".equalsIgnoreCase(user.getID()))
-		{
-			messageService.sendMessage(message.getChannel(),"https://pubg.me/yofu-kashi", message, true);
-		}
-		else if ("159729251707387904".equalsIgnoreCase(user.getID()))
-		{
-			messageService.sendMessage(message.getChannel(),"https://pubg.me/crytax", message, true);
-		}
-		else if("94917039277223936".equalsIgnoreCase(user.getID()))
-		{
-			messageService.sendMessage(message.getChannel(),"https://pubg.me/senpai-_-", message, true);
-		}
-		User newUser = sqliteClient.read(user.getID(), User.class);
-		*/
-	}	
+		messageService.sendMessage(message.getChannel(), messageToSend, message, true);
+
+	}
 }
